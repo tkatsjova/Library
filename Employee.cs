a9874c718666b6cb51f4a19407ea8ebf8ff87174
tkatsjova <80852049+tkatsjova@users.noreply.github.com>
2021-09-05 17:37:19 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary1
+{
+    public class Employee
+    {
+        public int EmpId;
+        public const string Enterprise = "ГУАП";
+        public string Name;
+    }
+}
